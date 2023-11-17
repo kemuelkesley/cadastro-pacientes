a7ad10e731840662a8e1dda1b1861e098d256dfd
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-11-17 17:50:49 -0300

    arquivos estaticos criados

--- setup/settings.py
+++ setup/settings.py
@@ -118,6 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# mostra onde todas as pastas de arquivos estáticos estão localizadas
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
