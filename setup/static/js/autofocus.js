1670f1116acbe7d11de8379afe2ec239ff3653b4
kemuelkesley <kemuelkesleytkd@outlook.com>
2023-12-02 14:38:45 -0300

    adicionado as fontes do google e mudança no modal e suas cores.

---
+++ setup/static/js/autofocus.js
@@ -0,0 +1,4 @@
+// colocar o foco no campo nome do formulário editar
+$(document).ready(function(){
+    $('#id_nome').focus();
+});
--- setup/static/js/botao-finalizar-modal.js
+++ setup/static/js/botao-finalizar-modal.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
-    // Alteração aqui: usamos uma classe para acionar o evento click
+
+$(document).ready(function(){    
     $('.finalizar-botao').click(function() {
         $('#confirmModal').modal('show');
     });
 
-    $('#confirmarBotao').click(function() {
-        // Aqui você pode adicionar a lógica para salvar as alterações (enviar o formulário) e redirecionar
+    $('#confirmarBotao').click(function() {       
         $('form').submit();
         $('#confirmModal').modal('hide');
     });
