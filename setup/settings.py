ffeeb2d5abb9cfe16609c4a0a78b7530b1949861
kemuelkesley <kemuelkesleytkd@outlook.com>
2026-03-31 14:55:36 -0300

    Criado nova configuração do .env e dependencias do projeto que não existia.

--- setup/settings.py
+++ setup/settings.py
@@ -12,20 +12,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 2. Carregue o arquivo .env
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxsn6tm)7q2b87dw9)-gp$)3%8f2+2tf=4b4%57frh0=*i$^*m'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
+# Coloca o dominio aqui.
 ALLOWED_HOSTS = []
 
 
@@ -93,11 +94,19 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # Banco de dados postegres.
